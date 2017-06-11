c31115cd1c56279df1113405f00e7e708fdf4651
Joshua Teng <joshteng@users.noreply.github.com>
2017-06-11 21:40:22 +0800

    Create run_one.sh

---
+++ run_one.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Ethereum miner for Microsoft Azure
+# Script by Ben Thompson
+echo -e "\e[31;43m***** Installing Drivers and Prerequisites *****\e[0m"
+sudo apt-get update
+sudo apt-get install software-properties-common gcc make
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install build-essential ubuntu-desktop -y
+sudo touch /etc/modprobe.d/nouveau.conf
+sudo echo 'blacklist nouveau\nblacklist lbm-nouveau' > /etc/modprobe.d/nouveau.conf
+sudo reboot
