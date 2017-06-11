b2d1a5391f65d06e96b7f776ab1cc9adfa28f919
Joshua Teng <joshteng@users.noreply.github.com>
2017-06-11 22:22:10 +0800

    Create run_two.sh

--- run_two.sh
+++ run_two.sh
@@ -1,3 +1,4 @@
+sudo systemctl stop lightdm.service
 wget -O NVIDIA-Linux-x86_64-367.92-grid.run https://go.microsoft.com/fwlink/?linkid=849941  
 chmod +x NVIDIA-Linux-x86_64-367.92-grid.run
 sudo ./NVIDIA-Linux-x86_64-367.92-grid.run
