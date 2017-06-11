f71c593715c91a756457a0bd018723459b39247b
Joshua Teng <joshteng@users.noreply.github.com>
2017-06-11 21:42:28 +0800

    Create run_two.sh

---
+++ run_two.sh
@@ -0,0 +1,7 @@
+wget -O NVIDIA-Linux-x86_64-367.92-grid.run https://go.microsoft.com/fwlink/?linkid=849941  
+chmod +x NVIDIA-Linux-x86_64-367.92-grid.run
+sudo ./NVIDIA-Linux-x86_64-367.92-grid.run
+
+sudo echo "\nIgnoreSP=TRUE\n" >> /etc/nvidia/gridd.conf.template
+
+sudo reboot
