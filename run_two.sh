1f0163cf4b9f8fa02d5bdc41440bb50cb0f4175d
Wai Hong Fong <waihong.fong@storehub.com>
2017-06-13 23:14:00 +0800

    Create run_two.sh

--- run_two.sh
+++ run_two.sh
@@ -1,5 +1,5 @@
 sudo systemctl stop lightdm.service
-wget -O NVIDIA-Linux-x86_64-367.92-grid.run https://go.microsoft.com/fwlink/?linkid=849941  
+wget -O nvidia-diag-driver-local-repo-ubuntu1604_375.66-1_amd64.deb https://www.dropbox.com/s/14m88n4ar9o6o4x/nvidia-diag-driver-local-repo-ubuntu1604_375.66-1_amd64.deb?dl=0
 sudo dpkg -i nvidia-diag-driver-local-repo-ubuntu1604_375.66-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda-drivers
