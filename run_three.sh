ce24955577c080edf507c9587dca62f0d1cfaee7
Wai Hong Fong <waihong.fong@storehub.com>
2017-06-13 23:36:08 +0800

    Create run_three.sh

--- run_three.sh
+++ run_three.sh
@@ -1,6 +1,9 @@
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install ethereum ethminer geth build-essential
-echo -e "\e[31;43m***** Generating Wallet: *****\e[0m"
-geth account new
+
+wget https://www.dropbox.com/s/4ucl9fv4gvcfe3y/claymore-v9.5.tar.gz?dl=0
+tar -xvzf claymore-v9.5.tar.gz
+
+echo -e "Mine CPU with ethminer -C -F http://eth.pool.minergate.com:55751/onesaviour@gmail.com --disable-submit-hashrate"
 echo -e "\e[31;43m***** Done! To start mining do: 'ethminer -G -F YOURPOOL --farm-recheck 200' *****\e[0m"
