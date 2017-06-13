94bdd9934074f25b14c476a22c5e4be3a1dee47d
Wai Hong Fong <waihong.fong@storehub.com>
2017-06-13 23:55:40 +0800

    Create start.bash

---
+++ start.bash
@@ -0,0 +1,2 @@
+cd Claymore\'s\ Dual\ Ethereum+Decred_Siacoin_Lbry_Pascal\ AMD+NVIDIA\ GPU\ Miner\ v9.5\ -\ LINUX/
+./ethdcrminer64 -epool us1.ethermine.org:4444 -ewal 0xAE21a7ffb9949D353aAFa99c5d0d9DBDa0DEB446.DigDug-NC06-XX -epsw x
