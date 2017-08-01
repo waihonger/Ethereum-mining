f265d89a30e012967fa91363b729286ebf82e250
Wai Hong Fong <waihong.fong@storehub.com>
2017-08-01 10:49:51 +0800

    Update run_three.sh

--- run_three.sh
+++ run_three.sh
@@ -11,3 +11,11 @@ sudo reboot
 
 echo -e "Mine CPU with : ethminer -C -F http://eth.pool.minergate.com:55751/onesaviour@gmail.com -t 5 --disable-submit-hashrate"
 echo -e "Add this to ./start.bash in claymore : ./ethdcrminer64 -epool us1.ethermine.org:4444 -ewal 0xAE21a7ffb9949D353aAFa99c5d0d9DBDa0DEB446.DigDug-NC06-XX -epsw x"
+
+
+cd /opt/Ethereum-mining/Claymore-miner
+wget https://www.dropbox.com/s/jq56rslvi21fzes/claymore-official97.tar.gz
+tar -xvzf claymore-official97.tar.gz
+cd Claymore\'s\ Dual\ Ethereum+Decred_Siacoin_Lbry_Pascal\ AMD+NVIDIA\ GPU\ Miner\ v9.7\ -\ LINUX/
+cp *.* ../ -Rf
+sudo reboot
